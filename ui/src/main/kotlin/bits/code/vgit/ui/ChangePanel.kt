95bf4add3e5feb8fcadbbf637823440af228e195
code-byte-labs <shihuanoffice@163.com>
2024-12-16 00:32:43 +0800

    use monaco font

--- ui/src/main/kotlin/bits/code/vgit/ui/ChangePanel.kt
+++ ui/src/main/kotlin/bits/code/vgit/ui/ChangePanel.kt
@@ -4,27 +4,36 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import bits.code.vgit.ui.style.Fonts
 
 @Suppress("FunctionName")
 @Composable
 fun ChangePanel(diffs: List<String>, close: () -> Unit) {
 
     Column(modifier = Modifier.fillMaxSize().background(Color.White)) {
-        Icon(
-            Icons.Filled.Close,
-            contentDescription = "",
-            modifier = Modifier.size(width = 20.dp, height = 20.dp).clickable { close() })
-        LazyColumn {
-            items(diffs.size, key = { diffs[it].hashCode() }) {
-                Text(diffs[it])
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Icon(
+                Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "",
+                modifier = Modifier.size(width = 20.dp, height = 20.dp).clickable { close() })
+        }
+
+        SelectionContainer {
+            LazyColumn {
+                items(diffs.size, key = { diffs[it].hashCode() }) {
+                    Text(text = diffs[it], style = TextStyle(fontFamily = Fonts.monaco, fontSize = 12.sp))
+                }
             }
         }
     }
---
+++ ui/src/main/kotlin/bits/code/vgit/ui/style/Fonts.kt
@@ -0,0 +1,8 @@
+package bits.code.vgit.ui.style
+
+import androidx.compose.ui.text.font.toFontFamily
+import androidx.compose.ui.text.platform.Font
+
+object Fonts {
+    val monaco = Font(resource = "Monaco.ttf").toFontFamily()
+}
